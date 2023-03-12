fc1836747784af63a27c672007452671c2a06cfd
satyalearn <mydigilearn2020@gmail.com>
2023-03-12 22:45:18 +0530

    project completed

--- src/components/Food.jsx
+++ src/components/Food.jsx
@@ -2,8 +2,25 @@ import React, { useState } from "react";
 import { data } from "../data/data.js";
 
 const Food = () => {
-  const [foods, setfoods] = useState(data);
+  const [foods, SetFoods] = useState(data);
   console.log(foods);
+
+  const filterType = (category) => {
+    SetFoods(
+      data.filter((item) => {
+        return item.category === category;
+      })
+    );
+  };
+
+  const filterPrice = (price) => {
+    SetFoods(
+      data.filter((item) => {
+        return item.price === price;
+      })
+    );
+  };
+
   return (
     <div className="max-w-[1640px] mx-auto px-4 py-12">
       <h1 className="text-orange-600 font-bold text-center text-4xl ">
@@ -16,19 +33,45 @@ const Food = () => {
         <div>
           <p className="font-bold text-gray-700">filter Type</p>
           <div>
-            <button className=" m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white">
-              Burgers
-            </button>
-            <button className=" m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white">
+            <button
+              onClick={() => {
+                SetFoods(data);
+              }}
+              className=" m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white"
+            >
               All
             </button>
-            <button className=" m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white">
+            <button
+              onClick={() => {
+                filterType("burgers");
+              }}
+              className=" m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white"
+            >
+              Burgers
+            </button>
+
+            <button
+              onClick={() => {
+                filterType("pizza");
+              }}
+              className=" m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white"
+            >
               Pizza
             </button>
-            <button className=" m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white">
+            <button
+              onClick={() => {
+                filterType("salad");
+              }}
+              className=" m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white"
+            >
               Salads
             </button>
-            <button className=" m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white">
+            <button
+              onClick={() => {
+                filterType("chicken");
+              }}
+              className=" m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white"
+            >
               Chicken
             </button>
           </div>
@@ -37,20 +80,64 @@ const Food = () => {
         <div>
           <p className="font-bold text-gray-700">Filter Price</p>
           <div className="flex justify-between max-w-[390px] w-full">
-            <button className=" m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white">
+            <button
+              onClick={() => {
+                filterPrice("$");
+              }}
+              className=" m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white"
+            >
               $
             </button>
-            <button className=" m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white">
+            <button
+              onClick={() => {
+                filterPrice("$$");
+              }}
+              className=" m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white"
+            >
               $$
             </button>
-            <button className=" m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white">
+            <button
+              onClick={() => {
+                filterPrice("$$$");
+              }}
+              className=" m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white"
+            >
               $$$
             </button>
-            <button className=" m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white">
+            <button
+              onClick={() => {
+                filterPrice("$$$$");
+              }}
+              className=" m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white"
+            >
               $$$$
             </button>
           </div>
         </div>
+      </div>
+      <div className="grid grid-cols-2 lg:grid-cols-4 gap-6 pt-4 ">
+        {foods.map((item, index) => {
+          return (
+            <div
+              key={index}
+              className="border shadow-lg hover:scale-105 duration-300"
+            >
+              <img
+                src={item.image}
+                alt={item.name}
+                className="w-full h-[200px] object-cover rounded-t-lg"
+              />
+              <div className="flex justify-between p-2 py-4 flex-wrap">
+                <p className="font-bold ">{item.name}</p>
+                <p>
+                  <span className="bg-orange-500 px-2 text-white p-1 rounded-full mt-2">
+                    {item.price}
+                  </span>
+                </p>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,58 +1,96 @@
-import React,{useState} from 'react'
-import {AiOutlineMenu, AiOutlineSearch, AiOutlineClose, AiFillTag} from 'react-icons/ai'
-import {BsFillCartFill, BsFillSaveFill} from 'react-icons/bs'
-import {TbTruckDelivery} from 'react-icons/tb'
-import {MdFavorite, MdHelp} from 'react-icons/md'
-import {FaWallet, FaUserFriends} from 'react-icons/fa'
-
-
-
+import React, { useState } from "react";
+import {
+  AiOutlineMenu,
+  AiOutlineSearch,
+  AiOutlineClose,
+  AiFillTag,
+} from "react-icons/ai";
+import { BsFillCartFill, BsFillSaveFill } from "react-icons/bs";
+import { TbTruckDelivery } from "react-icons/tb";
+import { MdFavorite, MdHelp } from "react-icons/md";
+import { FaWallet, FaUserFriends } from "react-icons/fa";
 
 const Navbar = () => {
-
-    const [nav, setNav] = useState(false)
+  const [nav, setNav] = useState(false);
   return (
-    <div className='flex justify-between  w-[100%] bg-red-200 p-2'>
-        <div className='flex items-center'>
-        <div className='cursor-pointer' onClick={()=>{setNav(!nav)}}>
-        <AiOutlineMenu  size={25}/>
+    <div className="flex justify-between  w-[100%] bg-black-200 p-2">
+      <div className="flex items-center">
+        <div
+          className="cursor-pointer"
+          onClick={() => {
+            setNav(!nav);
+          }}
+        >
+          <AiOutlineMenu size={25} />
         </div>
-        <h1 className='text-xl sm:text-2xl lg:text-3xl px-2'>Best <span className='font-bold'>Eats</span></h1>
-        <div className='flex items-center bg-gray-200 rounded-full p-1 text-[15px]'>
-            <p className='bg-black text-white p-2 rounded-full'>Delivery</p>
-            <p>Pickup</p>
+        <h1 className="text-xl sm:text-2xl lg:text-3xl px-2">
+          Best <span className="font-bold">Eats</span>
+        </h1>
+        <div className="flex items-center bg-gray-200 rounded-full p-1 text-[15px]">
+          <p className="bg-black text-white p-2 rounded-full">Delivery</p>
+          <p>Pickup</p>
         </div>
-        </div>
-        <div className='px-2 w-[300px] sm:w-[400px] md:w-[500px] bg-gray-200 rounded-full flex items-center py-2'>
-            <AiOutlineSearch size={20}/>
-            <input type="text" placeholder='search food' className='bg-transparent focus:outline-none px-2 w-full' />
-        </div>
-        <button className='bg-black rounded-full  hidden md:flex items-center text-white cursor-pointer'>
-        <BsFillCartFill /> <span className='px-1'>Cart</span>
-        </button>
+      </div>
+      <div className="px-2 w-[300px] sm:w-[400px] md:w-[500px] bg-gray-200 rounded-full flex items-center py-2">
+        <AiOutlineSearch size={20} />
+        <input
+          type="text"
+          placeholder="search food"
+          className="bg-transparent focus:outline-none px-2 w-full"
+        />
+      </div>
+      <button className="bg-black rounded-full  hidden md:flex items-center text-white cursor-pointer">
+        <BsFillCartFill /> <span className="px-1">Cart</span>
+      </button>
 
-        {nav && <div className='bg-black/80 fixed w-full h-screen z-10 top-0 left-0'>
-        </div>}
+      {nav && (
+        <div className="bg-black/80 fixed w-full h-screen z-10 top-0 left-0"></div>
+      )}
 
-        <div className={nav?'fixed top-0 left-0 w-[300px] h-screen z-10 bg-white duration-300': 'fixed top-0 left-[-100%] w-[300px] h-screen z-10 bg-white duration-300'}>
-            <AiOutlineClose className='absolute right-4 top-4' onClick={()=>{setNav(!nav
-                )}}/>
-            <h1 className='text-2xl px-2'>Best <span className='font-bold'>Eats</span></h1>
-            <nav>
-                <ul className='flex flex-col p-4 text-gray-800'>
-                    <li className='text-xl py-4 flex'><TbTruckDelivery  size={25} className='mr-4'/> Orders</li>
-                    <li className='text-xl py-4 flex'><MdFavorite  size={25} className='mr-4'/> Favourites</li>
-                    <li className='text-xl py-4 flex'><FaWallet  size={25} className='mr-4'/> Wallet</li>
-                    <li className='text-xl py-4 flex'><MdHelp  size={25} className='mr-4'/> Help</li>
-                    <li className='text-xl py-4 flex'><AiFillTag  size={25} className='mr-4'/> Promotions</li>
-                    <li className='text-xl py-4 flex'><BsFillSaveFill  size={25} className='mr-4'/> Best One</li>
-                    <li className='text-xl py-4 flex'><FaUserFriends size={25} className='mr-4'/> Invite Friends</li>
-
-                </ul>
-            </nav>
-        </div>
+      <div
+        className={
+          nav
+            ? "fixed top-0 left-0 w-[300px] h-screen z-10 bg-white duration-300"
+            : "fixed top-0 left-[-100%] w-[300px] h-screen z-10 bg-white duration-300"
+        }
+      >
+        <AiOutlineClose
+          className="absolute right-4 top-4"
+          onClick={() => {
+            setNav(!nav);
+          }}
+        />
+        <h1 className="text-2xl px-2">
+          Best <span className="font-bold">Eats</span>
+        </h1>
+        <nav>
+          <ul className="flex flex-col p-4 text-gray-800">
+            <li className="text-xl py-4 flex">
+              <TbTruckDelivery size={25} className="mr-4" /> Orders
+            </li>
+            <li className="text-xl py-4 flex">
+              <MdFavorite size={25} className="mr-4" /> Favourites
+            </li>
+            <li className="text-xl py-4 flex">
+              <FaWallet size={25} className="mr-4" /> Wallet
+            </li>
+            <li className="text-xl py-4 flex">
+              <MdHelp size={25} className="mr-4" /> Help
+            </li>
+            <li className="text-xl py-4 flex">
+              <AiFillTag size={25} className="mr-4" /> Promotions
+            </li>
+            <li className="text-xl py-4 flex">
+              <BsFillSaveFill size={25} className="mr-4" /> Best One
+            </li>
+            <li className="text-xl py-4 flex">
+              <FaUserFriends size={25} className="mr-4" /> Invite Friends
+            </li>
+          </ul>
+        </nav>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
